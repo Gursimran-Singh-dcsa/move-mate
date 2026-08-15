38d7358dca31b2f0bd3733221a670ab1db3a115a
Gursimran Singh <Gursimran@pardeepk4Q05N.vmware.com>
2026-08-15 18:09:58 +0530

    Add: changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-
+  basePath: "/move-mate",
   images: {
     unoptimized: true,
   },
