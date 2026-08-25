15d01dcf6076b27b0935b490e874fd18560aadd3
Gursimran Singh <Gursimran@pardeepk4Q05N.vmware.com>
2026-08-25 13:47:49 +0530

    Add: changes for GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,25 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/shared/components/header";
 import Footer from "@/shared/components/footer";
 
+const GTMScript = () => {
+  return (
+    <Script
+      id="gtm-script"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TKHSX3M9');`,
+      }}
+    />
+  );
+};
 export const metadata: Metadata = {
   metadataBase: new URL("https://movematerelocations.co.nz/"),
 
@@ -73,7 +89,18 @@ const roboto = Roboto({
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${roboto.variable} h-full antialiased`}>
+      <head>
+        <GTMScript />
+      </head>
       <body className="min-h-full flex flex-col  ">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TKHSX3M9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
         <Footer />
